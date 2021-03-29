3788d099faaf6fa940a49e9618b3e70f88fafb90
Will Whitty <tavurth@gmail.com>
2021-03-29 15:52:56 +0300

    Rotation and tool setup almost working

---
+++ RadialMenu/RadialMenu.gd
@@ -0,0 +1,52 @@
+tool
+extends ColorRect
+
+const MIN_WIDTH = 0.05
+
+export(float, 0, 1) var width_max = 1.0 setget set_width_max;
+export(float, 0, 1) var width_min = 0.5 setget set_width_min;
+
+export(float, 0, 3.1416) var cursor_size = 0.4 setget set_cursor_size;
+export(float, -3.1416, 3.1416) var cursor_deg = 0.4 setget set_cursor_deg;
+
+export(Color, RGBA) var color_bg = Color(0.4, 0.4, 0.4, 1.0) setget set_color_bg;
+export(Color, RGBA) var color_fg = Color(0.17, 0.69, 1.0, 1.0) setget set_color_fg;
+
+func set_shader_param(name: String, new_value):
+	self.material.set_shader_param(name, new_value)
+
+func set_width_max(new_value: float):
+	if new_value - MIN_WIDTH < 0: return
+
+	width_max = new_value
+	self.set_shader_param("width_max", new_value)
+
+	# Handle case where we're now smaller than the minimum size
+	if new_value < width_min + MIN_WIDTH:
+		self.set_width_min(new_value - MIN_WIDTH)
+
+func set_width_min(new_value: float):
+	if new_value + MIN_WIDTH > 1: return
+
+	width_min = new_value
+	self.set_shader_param("width_min", new_value)
+
+	# Handle case where we're now bigger than the minimum size
+	if new_value > width_min - MIN_WIDTH:
+		self.set_width_max(new_value + MIN_WIDTH)
+
+func set_cursor_deg(new_value: float):
+	cursor_deg = new_value
+	self.set_shader_param("cursor_deg", new_value)
+
+func set_cursor_size(new_value: float):
+	cursor_size = new_value
+	self.set_shader_param("cursor_size", new_value)
+
+func set_color_bg(new_value: Color):
+	color_bg = new_value
+	self.set_shader_param("color_bg", new_value)
+
+func set_color_fg(new_value: Color):
+	color_fg = new_value
+	self.set_shader_param("color_fg", new_value)
---
+++ RadialMenu/selector.shader
@@ -0,0 +1,46 @@
+shader_type canvas_item;
+
+const float PI_2 = 6.283185307;
+const vec2 MIDPOINT = vec2(0.5, 0.5);
+
+uniform float width_max = 1.0;
+uniform float width_min = 0.5;
+
+uniform float cursor_deg = 0.4;
+uniform float cursor_size = 0.4;
+
+uniform vec4 color_bg = vec4(0.4, 0.4, 0.4, 1.0); 
+uniform vec4 color_fg = vec4(0.17, 0.69, 1.0, 1.0); 
+
+float circle(vec2 uv, float radius) {
+  float dist = length(uv - MIDPOINT);
+
+  return 1. - smoothstep
+    (
+     radius - (radius * 0.01),
+     radius + (radius * 0.01),
+     (dist * dist) * 4.0
+    );
+}
+
+float should_discard(vec2 uv) {
+  return circle(uv, width_max) - circle(uv, width_min / 1.4);
+}
+
+float find_deg(vec2 uv) {
+  vec2 offset = uv - MIDPOINT;
+  return atan(offset.y, offset.x);
+}
+
+vec4 find_color(vec2 uv) {
+  float current_degree = abs(find_deg(uv) - cursor_deg);  
+  if (current_degree < cursor_size || current_degree > PI_2 - cursor_size) {
+    return color_fg;
+  }
+
+  return color_bg;
+}
+
+void fragment() {
+  COLOR = find_color(UV) * should_discard(UV);
+}

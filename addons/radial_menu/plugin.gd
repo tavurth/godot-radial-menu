c929d56a48dd58366d454f4662ded9953c758443
Iceflower <iceflower@gmx.de>
2022-03-06 19:54:17 +0100

    Improve plugin icon

--- addons/radial_menu/plugin.gd
+++ addons/radial_menu/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("RadialMenu", "Container", preload("RadialMenu.gd"), preload("icon.png"))
+	add_custom_type("RadialMenu", "Container", preload("RadialMenu.gd"), preload("icon.svg"))
 
 func _exit_tree():
 	remove_custom_type("RadialMenu")
